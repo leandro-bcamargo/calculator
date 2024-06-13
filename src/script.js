63e84f8d864453bce66249ad61df264c58bb2a0b
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-06-13 13:28:15 -0300

    Add initial implementation of basic operations

--- src/script.js
+++ src/script.js
@@ -20,7 +20,7 @@ let secondNum;
 let operator;
 let displayValue;
 
-function operate(operator, firstNum, secondNum) {
+function operate(firstNum, operator, secondNum) {
   switch(operator) {
     case "+": return add(firstNum, secondNum);
     case "-": return subtract(firstNum, secondNum);
@@ -30,20 +30,30 @@ function operate(operator, firstNum, secondNum) {
   }
 }
 
+function parseDisplay() {
+  const display = document.querySelector('#display');
+  const [firstNum, operator, secondNum] = display.value.split(/([+\-/*])/);
+  // console.log('firstNum:', firstNum, 'secondNum:', secondNum, 'operator:', operator);
+  display.value = '';
+  const newDisplayValue = operate(Number(firstNum), operator, Number(secondNum));
+  display.value = newDisplayValue;
+}
+
 function updateDisplay(e) {
-  const input = document.querySelector('#input');
+  const input = document.querySelector('#display');
   const inputValue = input.value;
   const btnValue = e.target.textContent;
-  console.log('btnValue:', btnValue);
   const result = inputValue === "0" ? btnValue : inputValue + btnValue;
-  console.log('result:', result);
   input.value = result;
   displayValue = result;
 }
 
 function configureBtnEventListeners() {
   const buttons = document.querySelectorAll('button');
-  buttons.forEach(btn => btn.addEventListener('click', (e) => updateDisplay(e)));
+  buttons.forEach(btn => {
+    if (btn.textContent !== '=') btn.addEventListener('click', (e) => updateDisplay(e))
+  });
 }
+
 
 window.onload = configureBtnEventListeners;
