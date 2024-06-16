1e1c441d5ea35d1a3d2477705a85e411dd0b360e
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-06-16 11:36:45 -0300

    Make progress towards doing successive operations

--- src/script.js
+++ src/script.js
@@ -15,10 +15,9 @@ function multiply(a, b) {
   return a * b;
 }
 
-let firstNum;
-let secondNum;
-let operator;
-let displayValue;
+const DIGITS = ['1','2','3','4','5','6','7','8','9','0'];
+const OPERATORS = ['+', '-', '/', '*'];
+let operatorsCounter = 0;
 
 function operate(firstNum, operator, secondNum) {
   switch(operator) {
@@ -30,20 +29,42 @@ function operate(firstNum, operator, secondNum) {
   }
 }
 
-function parseDisplay() {
+function calculate(trailingOperator) {
   const display = document.querySelector('#display');
   const [firstNum, operator, secondNum] = display.value.split(/([+\-/*])/);
-  // console.log('firstNum:', firstNum, 'secondNum:', secondNum, 'operator:', operator);
+  console.log('firstNum:', firstNum, 'secondNum:', secondNum, 'operator:', operator);
   display.value = '';
   const newDisplayValue = operate(Number(firstNum), operator, Number(secondNum));
-  display.value = newDisplayValue;
+  // console.log('newDisplayValue:', newDisplayValue)
+  display.value = newDisplayValue + trailingOperator;
 }
 
 function updateDisplay(e) {
   const input = document.querySelector('#display');
   const inputValue = input.value;
   const btnValue = e.target.textContent;
-  const result = inputValue === "0" ? btnValue : inputValue + btnValue;
+  let result;
+  if (inputValue === '0') {
+    result = btnValue;
+  } else if (DIGITS.includes(btnValue)) {
+    console.log('test')
+    result = inputValue + btnValue;
+  } else if (OPERATORS.includes(btnValue)) {
+    console.log('Apertou operador')
+    operatorsCounter++;
+    console.log('counter:', operatorsCounter)
+    if (operatorsCounter > 1) {
+      console.log('entrou')
+      calculate(btnValue);
+      const displayValue = document.querySelector('#display').value;
+      console.log('displayValue:', displayValue)
+      const includesOperator = displayValue.split('').some(char => OPERATORS.includes(char));
+      operatorsCounter = includesOperator ? 1 : 0;
+      return;
+    } else {
+      result = inputValue + btnValue;
+    }
+  }
   input.value = result;
   displayValue = result;
 }
